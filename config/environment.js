f1cf9542d47076410e07f6926d57ea5f1f36893f
Chris Freeman <cfreeman.code@gmail.com>
2017-04-06 23:06:30 -0500

    add `store` collection in module config

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,38 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'crates-status',
-    environment: environment
+    environment: environment,
+    // copied from https://github.com/glimmerjs/glimmer-application-pipeline/blob/master/lib/broccoli/default-module-configuration.ts
+    moduleConfiguration: {
+      types: {
+        application: { definitiveCollection: 'main' },
+        component: { definitiveCollection: 'components' },
+        helper: { definitiveCollection: 'components' },
+        renderer: { definitiveCollection: 'main' },
+        template: { definitiveCollection: 'components' }
+      },
+      collections: {
+        main: {
+          types: ['application', 'renderer']
+        },
+        store: {
+          unresolvable: true
+        },
+        components: {
+          group: 'ui',
+          types: ['component', 'template', 'helper'],
+          defaultType: 'component',
+          privateCollections: ['utils']
+        },
+        styles: {
+          group: 'ui',
+          unresolvable: true
+        },
+        utils: {
+          unresolvable: true
+        }
+      }
+    }
   };
 
   return ENV;
