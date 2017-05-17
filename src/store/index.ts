03570b459ed938807609b6b16176517a9f842492
Chris Freeman <cfreeman.code@gmail.com>
2017-05-16 20:28:13 -0500

    use the cratesiostatus twitter account

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 export default class Store {
   twitterUrl = "https://api.twitter.com/1.1/search/tweets.json"
 
-  user = "rustlang"
+  user = "cratesiostatus"
 
   // Probably more stuff will go here when we have more than a twitter feed
 }
